8822ae7bc970aae0a682bcf3f178b7826a564d1f
Yicha <1114637844@qq.com>
2020-10-09 11:53:35 +0800

    feat(controller):控制器方法参数修改为包装类型

--- src/main/java/com/gdut/builder/controller/IndexController.java
+++ src/main/java/com/gdut/builder/controller/IndexController.java
@@ -2,13 +2,16 @@ package com.gdut.builder.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
     @PostMapping("/generate")
-    public void generate(int questionNum,int maxLimit) {
+    @ResponseBody
+    public String generate(Integer questionNum,Integer maxLimit) {
         System.out.println("设定的题目数量为" + questionNum);
         System.out.println("题目中数值最大值为" + maxLimit);
+        return "操作成功";
     }
 }
